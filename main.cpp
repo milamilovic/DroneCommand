a3750c55cbef215369fca90c9d7cab3f7dac1daa
unknown <milamilovic03@gmail.com>
2024-11-20 17:29:06 +0100

    loading map

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-//Autor: Nedeljko Tesanovic
-//Opis: Primjer upotrebe tekstura
-
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
@@ -10,14 +7,12 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-//stb_image.h je header-only biblioteka za ucitavanje tekstura.
-//Potrebno je definisati STB_IMAGE_IMPLEMENTATION prije njenog ukljucivanja
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 unsigned int compileShader(GLenum type, const char* source);
 unsigned int createShader(const char* vsSource, const char* fsSource);
-static unsigned loadImageToTexture(const char* filePath); //Ucitavanje teksture, izdvojeno u funkciju
+static unsigned loadImageToTexture(const char* filePath);
 
 int main(void)
 {
@@ -33,9 +28,9 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window;
-    unsigned int wWidth = 800;
+    unsigned int wWidth = 1044;
     unsigned int wHeight = 800;
-    const char wTitle[] = "[Generic Title]";
+    const char wTitle[] = "Drone command";
     window = glfwCreateWindow(wWidth, wHeight, wTitle, NULL, NULL);
     
     if (window == NULL)
@@ -54,83 +49,89 @@ int main(void)
         return 3;
     }
 
-    unsigned int unifiedShader = createShader("basic.vert", "basic.frag");
+    unsigned int mapShader = createShader("texture.vert", "texture.frag");
+    unsigned int basicShader = createShader("basic.vert", "basic.frag");
 
-    float vertices[] =
-    {   //X    Y      S    T 
-        0.25, 0.0,   1.0, 0.0,//prvo tjeme
-        -0.25, 0.0,  0.0, 0.0, //drugo tjeme
-        0.0, 0.5,    0.5, 1.0 //trece tjeme
+    float map[] = {
+        // X    Y      S    T
+        0.98f,  0.98f,  1.0f, 1.0f, // Top-right
+        0.98f, -0.7f,  1.0f, 0.0f, // Bottom-right
+       -0.98f, -0.7f,  0.0f, 0.0f, // Bottom-left
+       -0.98f,  0.98f,  0.0f, 1.0f  // Top-left
     };
-    // notacija koordinata za teksture je STPQ u GLSL-u (ali se cesto koristi UV za 2D teksture i STR za 3D)
-    //ST koordinate u nizu tjemena su koordinate za teksturu i krecu se od 0 do 1, gdje je 0, 0 donji lijevi ugao teksture
-    //Npr. drugi red u nizu tjemena ce da mapira boje donjeg lijevog ugla teksture na drugo tjeme
+
+    unsigned int indices[] = {
+        0, 1, 3, // First triangle
+        1, 2, 3  // Second triangle
+    };
+
     unsigned int stride = (2 + 2) * sizeof(float);
 
-    unsigned int VAO;
+    unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-    unsigned int VBO;
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-   
+    glBufferData(GL_ARRAY_BUFFER, sizeof(map), map, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, stride, (void*)0);
     glEnableVertexAttribArray(0);
+
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void*)(2 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     
-    //Tekstura
-    unsigned checkerTexture = loadImageToTexture("res/texel_checker.png"); //Ucitavamo teksturu
-    glBindTexture(GL_TEXTURE_2D, checkerTexture); //Podesavamo teksturu
-    glGenerateMipmap(GL_TEXTURE_2D); //Generisemo mipmape 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);//S = U = X    GL_REPEAT, GL_CLAMP_TO_EDGE, GL_CLAMP_TO_BORDER
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);// T = V = Y
+    unsigned mapTexture = loadImageToTexture("res/majevica.png");
+    glBindTexture(GL_TEXTURE_2D, mapTexture);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);   //GL_NEAREST, GL_LINEAR
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glBindTexture(GL_TEXTURE_2D, 0);
-    glUseProgram(unifiedShader);
-    unsigned uTexLoc = glGetUniformLocation(unifiedShader, "uTex");
-    glUniform1i(uTexLoc, 0); // Indeks teksturne jedinice (sa koje teksture ce se citati boje)
+    glUseProgram(mapShader);
+    unsigned uTexLoc = glGetUniformLocation(mapShader, "uTex");
+    glUniform1i(uTexLoc, 0);
     glUseProgram(0);
-    //Odnosi se na glActiveTexture(GL_TEXTURE0) u render petlji
-    //Moguce je sabirati indekse, tj GL_TEXTURE5 se moze dobiti sa GL_TEXTURE0 + 5 , sto je korisno za iteriranje kroz petlje
 
-    while (!glfwWindowShouldClose(window))
-    {
-        
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        {
+    while (!glfwWindowShouldClose(window)) {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(window, GL_TRUE);
         }
-        
-        glClearColor(0.5, 0.5, 0.5, 1.0);
+
+        glClearColor(0.3f, 0.3f, 0.35f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(unifiedShader);
+        glUseProgram(mapShader);
         glBindVertexArray(VAO);
 
-        glActiveTexture(GL_TEXTURE0); //tekstura koja se bind-uje nakon ovoga ce se koristiti sa SAMPLER2D uniformom u sejderu koja odgovara njenom indeksu
-        glBindTexture(GL_TEXTURE_2D, checkerTexture);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, mapTexture);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glBindTexture(GL_TEXTURE_2D, 0);
         glBindVertexArray(0);
         glUseProgram(0);
-        
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
 
-    glDeleteTextures(1, &checkerTexture);
+    glDeleteTextures(1, &mapTexture);
     glDeleteBuffers(1, &VBO);
     glDeleteVertexArrays(1, &VAO);
-    glDeleteProgram(unifiedShader);
+    glDeleteProgram(mapShader);
 
     glfwTerminate();
     return 0;
@@ -217,10 +218,8 @@ static unsigned loadImageToTexture(const char* filePath) {
     unsigned char* ImageData = stbi_load(filePath, &TextureWidth, &TextureHeight, &TextureChannels, 0);
     if (ImageData != NULL)
     {
-        //Slike se osnovno ucitavaju naopako pa se moraju ispraviti da budu uspravne
         stbi__vertical_flip(ImageData, TextureWidth, TextureHeight, TextureChannels);
 
-        // Provjerava koji je format boja ucitane slike
         GLint InternalFormat = -1;
         switch (TextureChannels) {
         case 1: InternalFormat = GL_RED; break;
@@ -235,7 +234,6 @@ static unsigned loadImageToTexture(const char* filePath) {
         glBindTexture(GL_TEXTURE_2D, Texture);
         glTexImage2D(GL_TEXTURE_2D, 0, InternalFormat, TextureWidth, TextureHeight, 0, InternalFormat, GL_UNSIGNED_BYTE, ImageData);
         glBindTexture(GL_TEXTURE_2D, 0);
-        // oslobadjanje memorije zauzete sa stbi_load posto vise nije potrebna
         stbi_image_free(ImageData);
         return Texture;
     }
